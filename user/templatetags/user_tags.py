71d3fed0ca04e3ef8b08289525b902eaeb54c1de
dominator02 <55008447+dominator02@users.noreply.github.com>
2020-07-15 12:07:26 +0800

    修改qq登录回调url->uri

--- user/templatetags/user_tags.py
+++ user/templatetags/user_tags.py
@@ -9,7 +9,7 @@ def get_login_qq_url():
     params={
         'response_type':'code',
         'client_id':settings.QQ_APP_ID,
-        'redirect_url':settings.QQ_REDIRECT_URL,
+        'redirect_uri':settings.QQ_REDIRECT_URL,
         'state':settings.QQ_STATE,
     }
     return 'https://graph.qq.com/oauth2.0/authorize?'+urlencode(params)
