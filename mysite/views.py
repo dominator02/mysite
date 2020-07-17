189d36a9d206cb4646596ac49d30574ddb517613
dominator02 <55008447+dominator02@users.noreply.github.com>
2020-07-17 17:52:54 +0800

    实现站内简单搜索

--- mysite/views.py
+++ mysite/views.py
@@ -4,8 +4,9 @@ from blog.models import Blog
 from django.contrib.contenttypes.models import ContentType
 from django.utils import timezone
 import datetime
-from django.db.models import Sum
+from django.db.models import Sum,Q
 from django.core.cache import cache
+from django.core.paginator import Paginator
 
 
 def get_7_days_hot_blogs():
@@ -123,4 +124,36 @@ def home(request):
 #     context = {}
 #     return render(request, 'user_info.html', context)
 
+def search(request):
+    search_words=request.GET.get('wd','').strip()
+    condition=None
+    for word in search_words.split(' '):
+        if condition is None:
+            condition=Q(title__icontains=word)
+        else:
+            condition=condition | Q(title__icontains=word)
+    search_blogs=[]
+    if condition is not None:
+        search_blogs=Blog.objects.filter(condition)
 
+    page_num = request.GET.get('page', 1)
+    paginator = Paginator(search_blogs,10)
+    page_of_blogs = paginator.get_page(page_num)
+    current_page_num = page_of_blogs.number
+    # page_range=list(range(max(current_page_num-2,1),current_page_num))+list(range(current_page_num,min(current_page_num+2,paginator.num_pages)+1))
+    page_range = [x for x in range(current_page_num - 2, current_page_num + 3) if 0 < x <= paginator.num_pages]
+    if page_range[0] - 1 >= 2:
+        page_range.insert(0, '...')
+    if paginator.num_pages - page_range[-1] >= 2:
+        page_range.append('...')
+    if page_range[0] != 1:
+        page_range.insert(0, 1)
+    if page_range[-1] != paginator.num_pages:
+        page_range.append(paginator.num_pages)
+
+    context={}
+    context['search_words']=search_words
+    context['page_range'] = page_range
+    context['page_of_blogs']=page_of_blogs
+    context['search_blogs_count']=search_blogs.count()
+    return render(request,'search.html',context)
